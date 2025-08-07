17a0a74ea461e9f49dee390a8807a87c0c591ce5
Nishant Kumar <nishantkumar54321@example.com>
2025-08-07 07:15:10 +0530

    util code discussion

---
+++ db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB connected! DB host: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB connection FAILED:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
